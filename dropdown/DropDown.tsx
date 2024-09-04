ba6e16951a9bfb9f81bb16bbd78202cbcb43ff2c
g-imhoff <guillaume.imhoff170804@gmail.com>
2024-09-05 01:05:23 +0200

    improved the positioning of the dropdown content

--- dropdown/DropDown.tsx
+++ dropdown/DropDown.tsx
@@ -1,6 +1,6 @@
 export default function DropDown({ children }: { children: React.ReactNode }) {
   return (
-    <div id="DropDown" className="w-fit h-fit block">
+    <div id="DropDown" className="w-fit h-fit block relative">
       {children}
     </div>
   );
