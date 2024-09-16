44f66f481619d7aef14a6638c8d039146466cda1
g-imhoff <guillaume.imhoff170804@gmail.com>
2024-09-16 18:14:21 +0200

    added directly the alert animation into the alert component, in the case you put a position value, it will automatically use the default animation, if no position has been set, you can still do your own

--- components/alert/Alert.tsx
+++ components/alert/Alert.tsx
@@ -1,20 +1,19 @@
-import Image from "next/image";
-import { ComponentsProps } from "../../components";
+import { BasicProps, ComponentsProps } from "../../components";
 import "./style.css";
 import { getRadius } from "../../src/radius";
 import { getShadow } from "../../src/shadow";
+import { useId } from "react";
 
 interface AlertProps extends ComponentsProps {
   type?: "failed" | "success" | "warning";
   text: string;
-  svg: any;
-  svgAlt: string;
-  svgClose: any;
-  svgCloseAlt: string;
+  svg(props: BasicProps): JSX.Element;
+  svgClose(props: BasicProps): JSX.Element;
   svgCloseOnClick(
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
     id: string | undefined,
   ): void;
+  position?: "top-left" | "top-right" | "bottom-left" | "bottom-right";
 }
 
 function getAlertType(type?: string): string {
@@ -42,38 +41,97 @@ function getAlertType(type?: string): string {
   return color;
 }
 
+function getAlertPosition(position?: string): string | undefined {
+  if (position) {
+    switch (position) {
+      case "top-left":
+        return "alertDynamicShowTopLeft";
+      case "top-right":
+        return "alertDynamicShowTopRight";
+      case "bottom-left":
+        return "alertDynamicShowBottomLeft";
+      case "bottom-right":
+        return "alertDynamicShowBottomRight";
+    }
+  }
+}
+
 function getAlertStyle(props: AlertProps) {
   const type: string = getAlertType(props.type);
   const radius: string = getRadius(props.radius);
   const shadow: string = getShadow(props.shadow);
+  const position: string | undefined = getAlertPosition(props.position);
 
-  return type + " " + radius + " " + shadow;
+  return type + " " + radius + " " + shadow + " " + (position ? position : "");
 }
 
 export default function Alert(props: AlertProps) {
+  const id = props.id ? props.id : useId();
   const style: string = getAlertStyle(props);
+
+  function handleCloseAlert(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    id: string | undefined,
+    position?: "top-left" | "top-right" | "bottom-left" | "bottom-right",
+  ): void {
+    if (id) {
+      const element = document.getElementById(id);
+
+      if (position) {
+        switch (position) {
+          case "top-left":
+            if (element?.classList.contains("alertDynamicShowTopLeft")) {
+              element?.classList.remove("alertDynamicShowTopLeft");
+              element?.classList.add("alertDynamicHideTopLeft");
+            }
+            break;
+          case "top-right":
+            if (element?.classList.contains("alertDynamicShowTopRight")) {
+              element?.classList.remove("alertDynamicShowTopRight");
+              element?.classList.add("alertDynamicHideTopRight");
+            }
+            break;
+          case "bottom-left":
+            if (element?.classList.contains("alertDynamicShowBottomLeft")) {
+              element?.classList.remove("alertDynamicShowBottomLeft");
+              element?.classList.add("alertDynamicHideBottomLeft");
+            }
+            break;
+          case "bottom-right":
+            if (element?.classList.contains("alertDynamicShowBottomRight")) {
+              element?.classList.remove("alertDynamicShowBottomRight");
+              element?.classList.add("alertDynamicHideBottomRight");
+            }
+            break;
+        }
+      }
+    }
+
+    props.svgCloseOnClick(e, id);
+  }
+
   return (
     <div
-      id={props.id}
+      id={id}
       className={
         "subpixel-antialiased flex h-fit " + style + " " + props.className
       }
     >
-      <Image className="h-full m-4" src={props.svg} alt={props.svgAlt} />
+      {props.svg({
+        className: "h-full m-4 w-6 fill-none stroke-black dark:stroke-white",
+      })}
       <div className="flex justify-start items-center grow">
         <p className="subpixel-antialiased">{props.text}</p>
       </div>
       <button
         className="h-full w-fit"
         onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
-          props.svgCloseOnClick(e, props.id)
+          handleCloseAlert(e, id, props.position)
         }
       >
-        <Image
-          className="h-full m-4"
-          src={props.svgClose}
-          alt={props.svgCloseAlt}
-        />
+        {props.svgClose({
+          className: "h-full m-4 w-6 fill-none stroke-black dark:stroke-white",
+        })}
       </button>
     </div>
   );
